031c639b490bf4db5c3ddd3479cd4687ddee0143
trega trega <trega trega>
2017-03-14 22:50:52 +0100

    Added gradle files to CSV

---
+++ app/src/main/java/com/projects/trega/ichimokudroid/DataProvider/SqlHelper.java
@@ -0,0 +1,10 @@
+package com.projects.trega.ichimokudroid.DataProvider;
+
+import SQLiteOpenHelper;
+
+/**
+ * Created by tomek on 03.03.2017.
+ */
+
+public class SqlHelper extends SQLiteOpenHelper {
+}
